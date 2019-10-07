5cb36cec8a445be18182024a0f4d9a89fdc99a4f
Nicolas Albert <nicolas.albert@gmail.com>
2019-10-07 15:27:00 +0200

    Change USE_TUNNEL to TUNNEL_PORT and use it to choose the chisel port

--- mb-entrypoint.sh
+++ mb-entrypoint.sh
@@ -2,9 +2,13 @@
 
 if [ "$1" = "convertigo" ]; then
     
-    if [ "$USE_TUNNEL" = "YES" ]; then
-        /usr/local/bin/chisel server --port 28080 --reverse --socks5 --proxy http://localhost:28081 2>&1 >/var/log/chisel &
-        sed -i.bak2 -e 's/"28080"/"28081"/' $CATALINA_HOME/conf/server.xml
+    if [ "$TUNNEL_PORT" != "" ]; then
+        if [ "$TUNNEL_PORT" = "28080" ]; then
+            /usr/local/bin/chisel server --port 28080 --reverse --socks5 --proxy http://localhost:28081 2>&1 >/var/log/chisel &
+            sed -i.bak2 -e 's/"28080"/"28081"/' $CATALINA_HOME/conf/server.xml
+        else
+            /usr/local/bin/chisel server --port $TUNNEL_PORT --reverse --socks5 2>&1 >/var/log/chisel &
+        fi
     fi
     
     . /docker-entrypoint.sh convertigo
